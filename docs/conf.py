9373a29bab4d6ffd8a8e05c99f0533509f97fb80
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2024-09-17 17:19:51 -0400

    Up

--- docs/conf.py
+++ docs/conf.py
@@ -26,15 +26,13 @@ images_config = {
 templates_path = ['_templates']
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-# -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
+
 html_theme_options = {
     'prev_next_buttons_location': 'both',
     'sticky_navigation': True
 }
 html_static_path = ['_static']
 html_output_dir = '_build/html'
-
-# -- Options for EPUB output
 epub_show_urls = 'footnote'
